af71fc09e789947c402dbfaf88daa00c41411533
Tomer Shmueli <satlikus@gmail.com>
2022-09-06 17:18:24 +0300

    finished project coupon-system

--- CouponSystemUsingSpring-master/src/main/java/com/example/jb/Project2Againwoohoo/beans/ClientType.java
+++
@@ -1,7 +0,0 @@
-package com.example.jb.Project2Againwoohoo.beans;
-
-public enum ClientType {
-    ADMIN,
-    COMPANY,
-    CUSTOMER
-}
--- CouponSystemUsingSpring-master/src/main/java/com/example/jb/Project2Againwoohoo/exceptions/CustomExceptions.java
+++
@@ -1,7 +0,0 @@
-package com.example.jb.Project2Againwoohoo.exceptions;
-
-public class CustomExceptions extends Exception{
-    public CustomExceptions(ErrMsg message) {
-        super(message.getMessage());
-    }
-}
